d3f921fdc523468f47f955bba965e24eaf154893
Ruiyun <ruiyun.wen@gmail.com>
2013-07-19 11:01:34 +0800

    jna deps back to 3.4.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject Ruiyun/clj-native "0.9.3-SNAPSHOT"
   :description "Simplify usage of native libs from Clojure. Uses JNA.
                 Forked from bagucode/clj-native."
-  :dependencies [[net.java.dev.jna/jna "4.0.0"]]
+  :dependencies [[net.java.dev.jna/jna "3.4.0"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}})
