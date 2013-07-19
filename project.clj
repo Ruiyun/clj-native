76c5e768f37c4e0a36ac139bde1e37beac97068b
Ruiyun <ruiyun.wen@gmail.com>
2013-07-19 10:52:31 +0800

    dependencs up to date

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject clj-native "0.9.3-SNAPSHOT"
-  :description "Simplify usage of native libs from Clojure. Uses JNA."
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [net.java.dev.jna/jna "3.4.0"]])
+(defproject Ruiyun/clj-native "0.9.3-SNAPSHOT"
+  :description "Simplify usage of native libs from Clojure. Uses JNA.
+                Forked from bagucode/clj-native."
+  :dependencies [[net.java.dev.jna/jna "4.0.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}})
